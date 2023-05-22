755d2cd02c1cffa48c72898428b071eca56efbb3
vimalrathod82 <vimalrathod@vvsdtz.com>
2023-05-22 12:22:43 +0300

    changes on doctype fields

--- gas_order_management/gas_order_management/doctype/utility_sales_order/utility_sales_order.js
+++ gas_order_management/gas_order_management/doctype/utility_sales_order/utility_sales_order.js
@@ -2,7 +2,30 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Utility Sales Order', {
+	customer: function(frm) {
+		set_order_utility_detail(frm);
+	},
+    onload: function (frm) {
+		frm.get_field("order_utility_detail").grid.cannot_add_rows = true;
+    },
+    reload_items: function (frm){
+        set_order_utility_detail(frm);
+    }
 	// refresh: function(frm) {
 
 	// }
 });
+function set_order_utility_detail(frm){
+    frappe.db.get_doc("Customer",frm.doc.customer)
+    .then(doc => {
+        frm.clear_table("order_utility_detail");
+        for(const d of doc.customer_utility_detail) {
+            let row = frm.add_child("order_utility_detail", {
+                utility_type: d.utility_type,
+                utility_size: d.utility_size,
+                qty: 1
+            });
+        }
+        frm.refresh_field("order_utility_detail");
+	});
+}
